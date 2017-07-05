7f4b17e52e67775a5ad9c57a9170ac96e3fa7254
sdurighello <stefano.durighello@gmail.com>
2017-07-05 07:47:20 +0200

    rendered editor box

--- config/environment.js
+++ config/environment.js
@@ -20,6 +20,13 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    'ember-cli-summernote': {
+      "importBootstrapCSS": true,
+      "importBootstrapJS": true,
+      "importFontawesomeCSS": true,
+      "lang": "en-US", // "ko-KR" // "ru-RU" //"lang": "en-US"
     }
   };
 
